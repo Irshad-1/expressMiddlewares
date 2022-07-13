06ee53a11f6bed14f1623a34a369e7775e674923
Md Irshad <md14015@gmail.com>
2022-07-14 02:36:40 +0530

    done

---
+++ index.js
@@ -0,0 +1,78 @@
+const express = require("express");
+
+const app = express();
+
+let books = [
+  {
+    name: "javascript",
+    isbn: "9781593279509",
+    title: "Eloquent JavaScript, Third Edition",
+    subtitle: "A Modern Introduction to Programming",
+    author: "Marijn Haverbeke",
+    published: "2018-12-04T00:00:00.000Z",
+    publisher: "No Starch Press",
+    pages: 472,
+    description:
+      "JavaScript lies at the heart of almost every modern web application, from social apps like Twitter to browser-based game frameworks like Phaser and Babylon. Though simple for beginners to pick up and play with, JavaScript is a flexible, complex language that you can use to build full-scale applications.",
+    website: "http://eloquentjavascript.net/",
+  },
+  {
+    name: "modernjavscript",
+    isbn: "9781491943533",
+    title: "Practical Modern JavaScript",
+    subtitle: "Dive into ES6 and the Future of JavaScript",
+    author: "Nicolás Bevacqua",
+    published: "2017-07-16T00:00:00.000Z",
+    publisher: "O'Reilly Media",
+    pages: 334,
+    description:
+      "To get the most out of modern JavaScript, you need learn the latest features of its parent specification, ECMAScript 6 (ES6). This book provides a highly practical look at ES6, without getting lost in the specification or its implementation details.",
+    website: "https://github.com/mjavascript/practical-modern-javascript",
+  },
+  {
+    name: "ecmascript",
+    isbn: "9781593277574",
+    title: "Understanding ECMAScript 6",
+    subtitle: "The Definitive Guide for JavaScript Developers",
+    author: "Nicholas C. Zakas",
+    published: "2016-09-03T00:00:00.000Z",
+    publisher: "No Starch Press",
+    pages: 352,
+    description:
+      "ECMAScript 6 represents the biggest update to the core of JavaScript in the history of the language. In Understanding ECMAScript 6, expert developer Nicholas C. Zakas provides a complete guide to the object types, syntax, and other exciting changes that ECMAScript 6 brings to JavaScript.",
+    website: "https://leanpub.com/understandinges6/read",
+  },
+  {
+    name: "speakingjavascript",
+    isbn: "9781449365035",
+    title: "Speaking JavaScript",
+    subtitle: "An In-Depth Guide for Programmers",
+    author: "Axel Rauschmayer",
+    published: "2014-04-08T00:00:00.000Z",
+    publisher: "O'Reilly Media",
+    pages: 460,
+    description:
+      "Like it or not, JavaScript is everywhere these days -from browser to server to mobile- and now you, too, need to learn the language or dive deeper than you have. This concise book guides you into and through JavaScript, written by a veteran programmer who once found himself in the same position.",
+    website: "http://speakingjs.com/",
+  },
+];
+app.use(express.json());
+
+function allBooks(req, res, next) {
+  console.log("Fetching all books.");
+  next();
+}
+function singleBook(req, res) {
+  let query = req.params.name;
+  let answer = books.find((ele) => ele.name == query);
+  if (answer == undefined) res.status(404).send("No such book present");
+  res.send(answer);
+}
+
+app.get("/books/:name", singleBook);
+
+app.use(allBooks);
+app.get("/books", (req, res) => {
+  res.send(books);
+});
+app.listen(3000);
